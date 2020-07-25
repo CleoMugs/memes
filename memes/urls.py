15737eee167d4777bea3a97d57394e0c0554f146
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-25 22:36:31 +0300

    added api

--- memes/urls.py
+++ memes/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    path('api-auth/', include('rest_framework.urls')), #api
+    path('api/', include('accounts.api.urls'))
 
 ]
