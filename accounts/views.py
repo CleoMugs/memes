fe25b76d4e9a09d604f36154cd4baadc7c8f8fa6
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-26 13:12:06 +0300

    created static-folder

--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+	{
+		'blogger': 'Cleo',
+		'content': 'First post',
+		'date_created': 'July 26, 2020'
+	},
+
+	{
+		'blogger': 'Mugs',
+		'content': 'Second post',
+		'date_created': 'July 25, 2020'
+	}
+]
+
 
 def home(request):
-	return render(request, 'accounts/home.html')
+	context = {'posts': posts}
+	return render(request, 'accounts/home.html', context)
 
 
 def about(request):
