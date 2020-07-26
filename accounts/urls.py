7e152ab608222717122f041235861a71e625fb4e
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-26 16:49:52 +0300

    added register-page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='accounts-home'),
     path('about/', views.about, name='accounts-about'),
+    path('register/', views.register, name='accounts-register'),
+    path('login/', views.login, name='accounts-login'),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import *
 
 # Create your views here.
 
-posts = [
-	{
-		'blogger': 'Cleo',
-		'content': 'First post',
-		'date_created': 'July 26, 2020'
-	},
 
-	{
-		'blogger': 'Mugs',
-		'content': 'Second post',
-		'date_created': 'July 25, 2020'
-	}
-]
+def register(request):
+	form = UserCreationForm()
+	context = {'form': form}
+	return render(request, 'accounts/register.html', context)
+
+
+def login(request):
+	form = UserCreationForm()
+	context = {'form': form}
+	return render(request, 'accounts/login.html', context)
 
 
 def home(request):
