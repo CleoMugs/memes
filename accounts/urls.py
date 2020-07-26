cb9116812c3d7a3d30d06ffdb375bb7ba012d58e
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-26 22:32:52 +0300

    added user-authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='accounts-home'),
     path('about/', views.about, name='accounts-about'),
     path('register/', views.register, name='accounts-register'),
-    path('login/', views.login, name='accounts-login'),
+    path('login/', views.loginPage, name='accounts-login'),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+from django.contrib.auth import authenticate, login, logout
+
 
 from .models import *
 
@@ -15,7 +17,7 @@ def register(request):
 	if request.method == 'POST':
 		form = CreateUserForm(request.POST)
 		if form.is_valid():
-			#form.save()
+			form.save()
 			username = form.cleaned_data.get('username')
 			messages.success(request, f'Account created for {username}!')
 			return redirect('accounts-login')
@@ -24,9 +26,23 @@ def register(request):
 	return render(request, 'accounts/register.html', context)
 
 
-def login(request):
-	form = UserCreationForm()
-	context = {'form': form}
+def loginPage(request):
+	
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('accounts-home')
+
+		else:
+			messages.info(request, f'Username OR Password is incorrect')
+			return render(request, 'accounts/login.html')
+
+	context = {}
 	return render(request, 'accounts/login.html', context)
 
 
