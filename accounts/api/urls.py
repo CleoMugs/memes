203ffb2a94b7a0928ca9b3bb89f5bc6b48c9c384
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-30 22:40:29 +0300

    added css-styling

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -6,8 +6,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'ROLES', RoleViewSet)
 router.register(r'PROFILES', ProfileViewSet)
-#router.register(r'POSTS', PostViewSet)
+router.register(r'POSTS', PostViewSet)
 router.register(r'COMMENTS', CommentViewSet)
-#router.register(r'orders', OrderViewSet, basename='orders')
 
 urlpatterns = router.urls
